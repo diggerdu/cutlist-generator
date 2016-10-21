a24ad46c892620112ebe2519fe8d43c6956e016e
Fabian-Robert Stöter <faroit@users.noreply.github.com>
2016-10-21 12:06:05 +0200

    enable write output

--- generate_previews.py
+++ generate_previews.py
@@ -173,4 +173,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     dsd = dsdtools.DB()
-    generate_previews(dsd, write_estimates=False)
+    generate_previews(dsd, write_estimates=True)
